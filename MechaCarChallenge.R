1bf9f23f1572f17ceae45c694afe13744cdf282f
vbmartinez4 <vbmartinez15@gmail.com>
2022-09-11 17:32:19 -0400

    Create a summary statistic table for the suspension coil's PSI.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,8 @@
-# Deliverable 1: Linear Regression to Predict MPG
+# Download libraries.
 library(dplyr)
+library(tidyverse)
 
+# Deliverable 1: Linear Regression to Predict MPG
 # Import and read MechaCar_mpg.csv file as a dataframe.
 mecha_car <-read.csv(file='MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
 head(mecha_car)
@@ -10,3 +12,15 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mecha_car))
+
+# Deliverable 2: Create Visualizations for the Trip Analysis
+# Import and reach Suspension_Coil.csv file as a dataframe. 
+suspension_coil <-read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+head(suspension_coil)
+
+# Create a total_summary dataframe to get the mean, median, variance, and standard deviation of the PSI column.
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI),.groups = 'keep')
+
+# Create a lot_summary dataframe to group each manufacturing lot by the mean, median, 
+# variance, and standard deviation of the PSI column.
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI),.groups = 'keep')
